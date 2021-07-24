487775529c40d8d6aae34d97528c8a748ecd11b6
NaruDevnote <pokeraven4@gmail.com>
2021-07-24 03:19:54 +0100

    test: more testing

--- tests/general.test.ts
+++ tests/general.test.ts
@@ -1,5 +1,12 @@
+// This is NOT a usable code example, just testing
+
 import { Dashactyl } from '../src';
 
-const client = new Dashactyl('https://dynox.us/', '<fake_api_key>');
+const client = new Dashactyl('https://client.dynox.us/', 'some_api_key');
 
-(async () => console.log(client.ping()))();
+(async () => {
+    console.log(await client.ping(), 'ms');
+
+    const user = await client.users.fetch('622146791659405313');
+    console.log(user.tag);
+})();
