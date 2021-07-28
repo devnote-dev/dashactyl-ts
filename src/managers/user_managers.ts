b06ae0e16561582bb122cdcc4a7eb7fe8f127f05
NaruDevnote <pokeraven4@gmail.com>
2021-07-28 01:44:34 +0100

    feat(managers): formatting, typing fixes & new methods

--- src/managers/user_managers.ts
+++ src/managers/user_managers.ts
@@ -1,4 +1,5 @@
 import { Dashactyl } from '..';
+import { MAX_AMOUNT } from '.';
 import { DashUser, DashServer } from '../structures';
 
 class DashUserServerManager {
@@ -15,7 +16,7 @@ class DashUserServerManager {
 
     private _patch(data: object) {
         data['relationships']['servers']['data']
-        .forEach(o => {
+        .forEach((o: object) => {
             const server = new DashServer(this.client, o);
             this.cache.set(server.uuid, server);
             this.client.servers.cache.set(server.uuid, server);
@@ -24,17 +25,13 @@ class DashUserServerManager {
 
     public get(id: string): DashServer|null {
         if (!id) throw new Error();
-        for (const [key, val] of this.cache) {
-            if (key.includes(id)) return val;
-        }
-        return;
+        for (const [key, val] of this.cache) if (key.includes(id)) return val;
+        return null;
     }
 
     public find(fn: (value: DashServer, key: string) => boolean): DashServer|null {
-        for (const [key, val] of this.cache) {
-            if (fn(val, key)) return val;
-        }
-        return;
+        for (const [key, val] of this.cache) if (fn(val, key)) return val;
+        return null;
     }
 }
 
@@ -48,12 +45,50 @@ class CoinsManager {
         this.user = user;
         this.amount = data['coins'] || 0;
     }
+
+    public async add(amount: number): Promise<number> {
+        if (amount < 0 || amount > MAX_AMOUNT) throw new Error();
+
+        const res = await this.client._request('POST', '/api/addcoins', { id: this.user.username, amount });
+        if (res['status'] != 'success') throw new Error(res['status']);
+
+        this.amount += amount;
+        return this.amount;
+    }
+
+    public async remove(amount: number): Promise<number> {
+        if (amount < 0 || amount > MAX_AMOUNT) throw new Error();
+
+        amount = this.amount - amount;
+        if (amount < 0) amount = 0;
+
+        const res = await this.client._request('POST', '/api/setcoins', { id: this.user.username, amount });
+        if (res['status'] != 'success') throw new Error();
+
+        this.amount = amount;
+        return this.amount;
+    }
+
+    public async set(amount: number): Promise<number> {
+        if (amount < 0 || amount > MAX_AMOUNT) throw new Error();
+
+        const res = await this.client._request('POST', '/api/setcoins', { id: this.user.username, amount });
+        if (res['status'] != 'success') throw new Error(res['status']);
+
+        this.amount = amount;
+        return this.amount;
+    }
 }
 
 class ResourceManager {
     public client: Dashactyl;
     public user: DashUser;
-    public limits: { ram: 0, disk: 0, cpu: 0, servers: 0 };
+    public limits: {
+        ram: number;
+        disk: number;
+        cpu: number;
+        servers: number;
+    };
 
     constructor(client: Dashactyl, user: DashUser, data: object) {
         this.client = client;
@@ -73,6 +108,34 @@ class ResourceManager {
             this.limits.cpu += data['extra']['cpu'];
             this.limits.servers += data['extra']['servers'];
         }
+    }
+
+    public async set(
+        ram?: number,
+        disk?: number,
+        cpu?: number,
+        servers?: number
+    ): Promise<object> {
+        if (!ram && !disk && !cpu && !servers) throw new Error();
+        if (ram < 0 || ram > MAX_AMOUNT) throw new Error();
+        if (disk < 0 || disk > MAX_AMOUNT) throw new Error();
+        if (cpu < 0 || cpu > MAX_AMOUNT) throw new Error();
+        if (servers < 0 || servers > 10) throw new Error();
+
+        const res = await this.client._request(
+            'POST', '/api/setresources',
+            { id: this.user.username, ram, disk, cpu, servers }
+        );
+        if (res['status'] != 'success') throw new Error(res['status']);
+
+        this.limits = { ram, disk, cpu, servers };
+        return this.limits;
+    }
+
+    public async setPlan(plan?: string): Promise<boolean> {
+        const res = await this.client._request('POST', '/api/setplan', { id: this.user.username, package: plan });
+        if (res['status'] != 'success') throw new Error(res['status']);
+        return true;
     }
 }
 
