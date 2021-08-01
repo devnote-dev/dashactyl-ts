8b4237f7c7af6b21a074c80c61bd5239963e6ddb
NaruDevnote <pokeraven4@gmail.com>
2021-08-01 18:30:07 +0100

    refactor(user): extend from meta user & update endpoint

--- src/structures/dashuser.ts
+++ src/structures/dashuser.ts
@@ -1,64 +1,21 @@
-import { Dashactyl } from '..';
+import { Dashactyl, MetaDashUser } from '..';
 import {
     DashUserServerManager,
     CoinsManager,
     ResourceManager
 } from '../managers';
 
-/**
- * Represents a Pterodactyl user.
- */
-class DashUser {
-    public client: Dashactyl;
-    public id: number;
-    public uuid: string;
-    public isAdmin: boolean;
-
-    public username: string;
-    public email: string;
-    public firstname: string;
-    public lastname: string;
-
-    public language: string;
-    public tfa: boolean;
-
-    public createdAt: Date;
-    public createdTimestamp: number;
-    public updatedAt: Date | null;
-    public updatedTimestamp: number | null;
-
+class DashUser extends MetaDashUser {
     public servers: DashUserServerManager;
     public resources: ResourceManager;
     public coins: CoinsManager;
 
     constructor(client: Dashactyl, data: object) {
-        const att = data['userinfo']['attributes'];
+        super(client, data);
 
-        this.client = client;
-        this.id = att['id'];
-        this.uuid = att['uuid'];
-        this.isAdmin = att['root_admin'] || false;
-        
-        this.username = att['username'];
-        this.email = att['email'];
-        this.firstname = att['first_name'];
-        this.lastname = att['last_name'];
-
-        this.language = att['language'];
-        this.tfa = att['2fa'];
-
-        this.createdAt = new Date(att['created_at']);
-        this.createdTimestamp = this.createdAt.getTime();
-        this.updatedAt = att['updated_at'] !== null ? new Date(att['updated_at']) : null;
-        this.updatedTimestamp = this.updatedAt ? this.updatedAt.getTime() : null;
-
-        this.servers = new DashUserServerManager(this.client, this, att);
+        this.servers = new DashUserServerManager(this.client, this, data['userinfo']['attributes']);
         this.resources = new ResourceManager(this.client, this, data);
         this.coins = new CoinsManager(this.client, this, data);
-    }
-
-    get tag(): string {
-        return this.firstname + this.lastname
     }
 
     /**
@@ -66,7 +23,7 @@ class DashUser {
      * @returns {Promise<void>}
      */
     async remove(): Promise<void> {
-        const res = await this.client._request('POST', '/api/removeaccount', { id: this.username });
+        const res = await this.client._request('DELETE', `/api/users/${this.username}`);
         if (res['status'] !== 'success') throw new Error(res['status']);
         this.client.users.cache.delete(this.uuid);
     }
