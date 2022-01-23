a9927ce06840b3fca7dfed5ed8fdca3d0c791dda
devnote-dev <pokeraven4@gmail.com>
2022-01-23 04:32:11 +0000

    chore(*): remove old files

--- src/managers/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './user_managers';
-export * from './client_managers';
-
-export const MAX_AMOUNT = 999_999_999_999_999;
--- src/structures/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './MetaDashUser';
-export * from './DashServer';
-export * from './DashUser';
-export * from './Coupon';
-export * from './Client';
