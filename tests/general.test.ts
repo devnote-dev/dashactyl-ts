770727f1f58191411c6e211e09c95fa74f7dcb1b
devnote-dev <pokeraven4@gmail.com>
2022-01-23 07:40:07 +0000

    chore: fix test

--- tests/general.test.ts
+++ tests/general.test.ts
@@ -1,12 +1,7 @@
 // This is NOT a usable code example, just testing
 
-import { Dashactyl } from '../src';
+import Client from '../src/client';
 
-const client = new Dashactyl('https://client.dynox.us/', 'some_api_key');
+const client = new Client('https://some.good.host', 'some_api_key');
 
-(async () => {
-    console.log(await client.ping(), 'ms');
-
-    const user = await client.users.fetch('622146791659405313');
-    console.log(user.tag);
-})();
+client.users.fetch(5).then(console.log);
